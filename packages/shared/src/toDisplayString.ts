84c01366f6afcf36304c9b94c9513a8b7f77a2b3
lowerfish <wangjianzhong1990@gmail.com>
2020-10-13 14:04:41 +0800

    docs(shared): shared 完成

--- packages/shared/src/toDisplayString.ts
+++ packages/shared/src/toDisplayString.ts
@@ -4,6 +4,8 @@ import { isArray, isMap, isObject, isPlainObject, isSet } from './index'
  * For converting {{ interpolation }} values to displayed strings.
  * @private
  */
+// 将插值元素转换为字符串
+// 对象使用 JSON.stringify 转换;非对象使用 String 强转
 export const toDisplayString = (val: unknown): string => {
   return val == null
     ? ''
